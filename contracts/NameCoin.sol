cc5a0ebed4d6382c96ecd66c5380f6d0358cd4a2
Trylastsky <deleron972@gmail.com>
2024-04-16 23:19:41 +0400

    ads

--- contracts/NameCoin.sol
+++ contracts/NameCoin.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract NameCoin is ERC20 {
 
     address payable public owner;
-    uint price = 100; // цена за 1000 токенов
+    uint price = 1; // цена за 1000 токенов
 
     constructor() ERC20("NameCoin", "NC") {
         owner = payable(msg.sender);
@@ -22,15 +22,15 @@ contract NameCoin is ERC20 {
     }
 
     
-//     function buyToken(uint256 _value) public payable {
-//     uint256 tokenAmount = _value * 1000;    // Вычисляем количество токенов для передачи
-//     uint256 ethValue = _value * 1 ether;  // Проверяем, что отправленное количество ETH достаточно для покупки токенов
-//     require(msg.value >= ethValue, "Insufficient ETH sent");
-//     _transfer(owner, msg.sender, tokenAmount);    // Выполняем перевод токенов
-//     owner.transfer(msg.value);  // Переводим эфирные средства на адрес владельца контракта
+    function buyToken(uint256 _value) public payable {
+    uint256 tokenAmount = _value * 1000;    // Вычисляем количество токенов для передачи
+    uint256 ethValue = _value * 1 ether;  // Проверяем, что отправленное количество ETH достаточно для покупки токенов
+    require(msg.value >= ethValue, "Insufficient ETH sent");
+    _transfer(owner, msg.sender, tokenAmount);    // Выполняем перевод токенов
+    owner.transfer(msg.value);  // Переводим эфирные средства на адрес владельца контракта
    
 
-// }
+}
 
 
 
